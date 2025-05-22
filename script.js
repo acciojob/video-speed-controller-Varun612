ad5c37972a158f2231b090c7f5ec2808658d294a
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-22 10:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const inputs = document.querySelectorAll('.controls input');
+const toggleBtn = document.getElementById('toggle');
+const video = document.getElementById('video');
+const volumeBtn = document.getElementById('volume');
+const playbackSpeedBtn = document.getElementById('playbackSpeed');
+const rewindBtn = document.getElementById('rewind');
+const forwardBtn = document.getElementById('forward');
+const progressFilled = document.querySelector('progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+toggleBtn.addEventListener('click',()=> {
+	if(video.paused) {
+		video.play();
+		toggleBtn.textContent = `❚ ❚`;
+	} else {
+		video.pause();
+		toggleBtn.textContent = `►`;
+	}	
+});
+
+volumeBtn.addEventListener('click',()=>{
+	video.volume=volumeBtn.value;
+});
+playbackSpeedBtn.addEventListener('click',()=>{
+	video.playbackRate=playbackSpeedBtn.value;
+});
+rewindBtn.addEventListener('click',()=>{
+	video.currentTime-=10;
+});
+forwardBtn.addEventListener('click',()=>{
+	video.currentTime+=25;
+});
+video.addEventListener('timeUpdate', ()=> {
+	const percent = (video.currentTime/video.duration) *100;
+	progressFilled.style.width=`${percent}`;
+});
